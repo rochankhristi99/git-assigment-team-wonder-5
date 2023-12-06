120cd348b0bd7785f01b6f757446db97aad14682
rochankhristi99 <rochankhristi99@gmail.com>
2023-12-06 23:05:56 +0200

    First I have created Repository and invite my team members.Apart from this, I have Created Addition and Subtraction method and Main method.

---
+++ src/Calculator.java
@@ -0,0 +1,23 @@
+
+public class Calculator {
+    
+    // Addition method
+       public String add(int a, int b) {
+           int result = a + b;
+           return "Addition: " + a + " + " + b + " = " + result;
+       }
+
+       // Subtraction method
+       public String subtract(int a, int b) {
+           int result = a - b;
+           return "Subtraction: " + a + " - " + b + " = " + result;
+       }
+
+    public static void main(String[] args) {
+
+        Calculator calculator = new Calculator();
+        System.out.println(calculator.add(5, 3));
+        System.out.println(calculator.subtract(10, 4));
+    }
+
+}
