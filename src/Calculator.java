68d7befa884720d7a80baa4076763ebacf4092cc
rochankhristi99 <rochankhristi99@gmail.com>
2023-12-07 22:21:59 +0200

    I have create another two methods,one for multiplication and second for division.

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,21 +2,43 @@
 public class Calculator {
     
     // Addition method
-       public String add(int a, int b) {
+       public String add(int a, int b) 
+       {
            int result = a + b;
            return "Addition: " + a + " + " + b + " = " + result;
        }
 
        // Subtraction method
-       public String subtract(int a, int b) {
+       public String subtract(int a, int b) 
+       {
            int result = a - b;
            return "Subtraction: " + a + " - " + b + " = " + result;
        }
 
-    public static void main(String[] args) {
+       // Multiplication method
+       public String multiply(int a, int b)
+       {
+           int result = a * b;
+           return "Multiplication: " + a + " * " + b + " = " + result;
+       }
+
+       // Division method
+       public String divide(int a, int b)
+       {
+           if (b == 0) {
+               throw new IllegalArgumentException("Cannot divide by zero");
+           }
+           double result = (double) a / b;
+           return "Division: " + a + " / " + b + " = " + result;
+       }
+    
+    public static void main(String[] args) 
+    {
 
         Calculator calculator = new Calculator();
         System.out.println(calculator.add(5, 3));
         System.out.println(calculator.subtract(10, 4));
+        System.out.println(calculator.multiply(6, 7));
+        System.out.println(calculator.divide(15, 3));
     }
 }
